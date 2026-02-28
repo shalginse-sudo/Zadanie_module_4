a3069f62960cb89030bdf31c17f17109a0f46937
Sergey Shalygin <shalyginse@yandex.ru>
2026-02-28 16:26:13 +0300

    add Zadanie_1-2

---
+++ Lesson 1/Zadanie_1-2/Zadanie_1-2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+
+
+int func(int mas[], int size, int zhach)
+{
+    int left = 0;
+    int right = size;
+    while (left < right)
+    {
+        int a = left + (right - left) / 2;
+        if (mas[a] <= zhach)
+        {
+            left = a + 1;
+        }
+        else { right = a; }
+    }
+    int rez = size - left;
+    return rez;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int mas[]{ 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+    int size = sizeof(mas) / sizeof(mas[0]);
+    int start = 0;
+
+    std::cout << "Введите точку отсчета: ";
+    std::cin >> start;
+    std::cout << "Количество элементов в массиве, больших чем " << start << ": " << func(mas, size, start);
+    std::cout << std::endl;
+}
+
