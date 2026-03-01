ebbc554aed6acc54d850548d40e6abcf9401a35c
Sergey Shalygin <shalyginse@yandex.ru>
2026-03-01 18:06:07 +0300

    add Zadanie 2-1,2-2

---
+++ Lesson 2/Zadanie 2-1/Zadanie 2-1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <windows.h>
+
+
+long long fibonache(int nomer)
+{
+    if (nomer == 0)
+        return 0;
+    if (nomer == 1)
+        return 1;
+    return fibonache(nomer - 1) + fibonache(nomer - 2);
+}
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int userInput = 0;
+    std::cout << "Введите число: ";
+    std::cin >> userInput;
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < userInput; i++)
+    {
+        long long fib = fibonache(i);
+        std::cout << fib << ' ';
+    }
+    std::cout << std::endl;
+}
---
+++ Lesson 2/Zadanie 2-2/Zadanie 2-2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <windows.h>
+
+
+long long* memo; // Указатель на динамическую память для хранения чисел Фибоначчи
+
+long long fibonache(int nomer)
+{
+    // Если значение уже вычислено, возвращаем его из памяти
+    if (memo[nomer] != -1)
+        return memo[nomer];
+
+    // Вычисляем новое значение и сохраняем в память
+    if (nomer == 0)
+        memo[nomer] = 0;
+    else if (nomer == 1)
+        memo[nomer] = 1;
+    else
+        memo[nomer] = fibonache(nomer - 1) + fibonache(nomer - 2);
+
+    return memo[nomer];
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int userInput = 0;
+    std::cout << "Введите число: ";
+    std::cin >> userInput;
+
+    // Создаём динамический массив нужного размера
+    memo = new long long[userInput];
+
+    // Инициализация всех элементов массива значениями "-1"
+    for (int i = 0; i < userInput; i++) 
+    {
+        memo[i] = -1;
+    }
+
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < userInput; i++)
+    {
+        long long fib = fibonache(i);
+        std::cout << fib << ' ';
+    }
+    std::cout << std::endl;
+
+    delete[] memo;
+
+    return 0;
+}
